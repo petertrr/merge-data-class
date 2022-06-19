d1e19581b89aac938cab7880d5ef44b04f19b89f
Peter Trifanov <peter.trifanov@mail.ru>
2022-06-19 14:56:08 +0300

    feat: Add basic tests

--- merge-data-class-ksp/build.gradle.kts
+++ merge-data-class-ksp/build.gradle.kts
@@ -10,4 +10,14 @@ dependencies {
 
     ksp("dev.zacsweers.autoservice:auto-service-ksp:1.0.0")
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
+
+    testImplementation(libs.junit.jupiter.engine)
+    testImplementation(libs.kotest.assertions)
+    testImplementation(libs.tschuchortdev.testing)
+    testImplementation(libs.tschuchortdev.testing.ksp)
+    testImplementation(project(":merge-data-class-annotations", "jvmArchive"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }
