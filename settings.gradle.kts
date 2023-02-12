8a124b5f927b623962fb5ce8659f6faea7034a4b
Peter Trifanov <peter.trifanov@gmail.com>
2023-02-12 13:43:02 +0300

    build: Move everything Gradle-related into gradle directory; enable build scans (#4)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,21 @@
 import org.gradle.api.internal.FeaturePreviews.Feature
 
-rootProject.name = "merge-data-class"
-includeBuild("build-logic")
-include("merge-data-class-annotations")
-include("merge-data-class-ksp")
-
 pluginManagement {
     repositories {
         mavenCentral()
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("com.gradle.enterprise") version "3.12.3"
+}
+
+rootProject.name = "merge-data-class"
+includeBuild("gradle/build-logic")
+include("merge-data-class-annotations")
+include("merge-data-class-ksp")
+
 dependencyResolutionManagement {
     repositories {
         mavenCentral()
@@ -18,3 +23,14 @@ dependencyResolutionManagement {
 }
 
 enableFeaturePreview(Feature.TYPESAFE_PROJECT_ACCESSORS.name)
+
+gradleEnterprise {
+    @Suppress("AVOID_NULL_CHECKS")
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
